4bf0291eaecfcadb8e6f1caef97abf0940a60f01
Jude Fernandes <jude.manutd@gmail.com>
2019-11-14 23:43:02 +0530

    used public hosting for docs used redocs removed swagger ui express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,6 @@
 import "module-alias/register";
 import * as functions from "firebase-functions";
 import server from "./app";
-import docsServer from "./docs";
 import { connectToServer } from "./utils/db";
 
 connectToServer(functions);
@@ -12,9 +11,3 @@ export const api = functions
     memory: "2GB",
   })
   .https.onRequest(server);
-
-export const docs = functions
-  .runWith({
-    memory: "2GB",
-  })
-  .https.onRequest(docsServer);
